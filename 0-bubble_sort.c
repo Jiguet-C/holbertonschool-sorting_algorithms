8b0782e491527e2d7e6e39a73e066e2b73e43864
Stefani-web <stephaniecvlho@gmail.com>
2024-04-11 19:02:28 +0200

    correct errors checker

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void bubble_sort(int *array, size_t size)
 {
 	int temp;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size == 0)
 		return;
 
 	for (size_t index = 0; index < size - 1; index++)
