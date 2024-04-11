d9eab45fa6ec4048421d9e28bb6da77b5238e93b
Stefani-web <stephaniecvlho@gmail.com>
2024-04-11 18:18:43 +0200

    sorts an array of integers using the Bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using the Bubble sort algorithm
+ * @array: Print, after each time you swap two elements
+ * @size: Size of array
+ *
+ * Return: void
+ */
+
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (size_t index = 0; index < size - 1; index++)
+	{
+		for (size_t idx = 0; idx < size - index - 1; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
